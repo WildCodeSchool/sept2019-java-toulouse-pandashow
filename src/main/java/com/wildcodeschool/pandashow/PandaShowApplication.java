4a8906eb05728f324458cf6e2c42f086c406b3ba
bastienwcs <email>
2019-10-18 13:12:00 +0200

    init

---
+++ src/main/java/com/wildcodeschool/pandashow/PandaShowApplication.java
@@ -0,0 +1,13 @@
+package com.wildcodeschool.pandashow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PandaShowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PandaShowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wildcodeschool/pandashow/PandaShowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wildcodeschool.pandashow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PandaShowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
