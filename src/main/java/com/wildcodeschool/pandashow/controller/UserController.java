3ed913dc211588694b27e73ba64ba4ccaa96dbcd
jonezime <jonezime@gmail.com>
2019-11-12 13:20:18 +0100

    update user-controller

--- src/main/java/com/wildcodeschool/pandashow/controller/UserController.java
+++ src/main/java/com/wildcodeschool/pandashow/controller/UserController.java
@@ -3,12 +3,13 @@ package com.wildcodeschool.pandashow.controller;
 import com.wildcodeschool.pandashow.entity.User;
 import com.wildcodeschool.pandashow.repository.UserRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-public class SignInController {
+public class UserController {
 
     private UserRepository repository = new UserRepository();
 
@@ -20,10 +21,26 @@ public class SignInController {
     @PostMapping("/sign")
     public String connexion(@RequestParam String username, @RequestParam String password) {
         User user = repository.getByUsername(username, password);
-        if (user != null) {
-            return "redirect:/sign";
-        }
         return "mylist";
+    }
+
+    @GetMapping("/join")
+    public String join() {
+        return "join";
+    }
+
+    @PostMapping("/join")
+    public String userUpdate(Model model,
+                             @RequestParam String username,
+                             @RequestParam String password,
+                             @RequestParam String passwordConfirmation,
+                             @RequestParam String email
+    ) {
+        if (!password.equals(passwordConfirmation)) {
+            return "redirect:/join";
+        }
+        model.addAttribute("user", repository.saveUser(username, password, email));
+        return "redirect:/mylist";
     }
 }
 
