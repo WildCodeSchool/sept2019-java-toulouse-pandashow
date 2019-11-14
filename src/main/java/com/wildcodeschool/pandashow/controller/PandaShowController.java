9d6b0ff7277a970b6c1bccbf1028c636da30fa50
Julien Onézime <jonezime@gmail.com>
2019-11-14 10:22:42 +0100

    create direction visitor/user

--- src/main/java/com/wildcodeschool/pandashow/controller/PandaShowController.java
+++ src/main/java/com/wildcodeschool/pandashow/controller/PandaShowController.java
@@ -20,5 +20,4 @@ public class PandaShowController {
     public String account() {
         return "account";
     }
-
 }
--- src/main/java/com/wildcodeschool/pandashow/controller/TvShowController.java
+++ src/main/java/com/wildcodeschool/pandashow/controller/TvShowController.java
@@ -1,12 +1,14 @@
 package com.wildcodeschool.pandashow.controller;
 
 import com.wildcodeschool.pandashow.entity.TvShow;
+import com.wildcodeschool.pandashow.entity.User;
 import com.wildcodeschool.pandashow.repository.TvShowRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpSession;
 import java.util.List;
 
 @Controller
@@ -23,11 +25,16 @@ public class TvShowController {
         return "discover-shows";
     }
 
-    @GetMapping("/show-visitor")
-    public String visitorShows(Model out, @RequestParam Long id) {
+    @GetMapping("/show-details")
+    public String showDetails(HttpSession session, Model out, @RequestParam Long id) {
 
         TvShow show = repository.findById(id);
         out.addAttribute("showDetails", show);
+
+        if(session.getAttribute("currentUser") != null) {
+
+            return "show-user";
+        }
 
         return "show-visitor";
     }
